721f649ba63290db4fb4d6d36c117d554686eba3
mike <mpakleung@gmail.com>
2019-09-17 02:01:20 -0400

    Updates

--- precache-manifest.9729f24faefdf44829f8d29f0263ccf8.js
+++ precache-manifest.9729f24faefdf44829f8d29f0263ccf8.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5447de0ba7a3a4fb73c8953d13d5d675",
+    "revision": "a401e94f13bb27109112c3939ae20260",
     "url": "/snaplifts/index.html"
   },
   {
-    "revision": "9616153201cd40c3258c",
-    "url": "/snaplifts/static/css/main.37f2c406.chunk.css"
+    "revision": "e4e5f529612b43d57545",
+    "url": "/snaplifts/static/css/main.b671751d.chunk.css"
   },
   {
     "revision": "9eee67cf782a38d35aeb",
     "url": "/snaplifts/static/js/2.d6a534e5.chunk.js"
   },
   {
-    "revision": "9616153201cd40c3258c",
-    "url": "/snaplifts/static/js/main.8972b0c8.chunk.js"
+    "revision": "e4e5f529612b43d57545",
+    "url": "/snaplifts/static/js/main.13a6bb99.chunk.js"
   },
   {
     "revision": "aedb45d7df3483200b5e",
