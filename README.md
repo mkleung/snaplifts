f5ece8ece2062e953542a080fb4b961ed7438fae
mike <mpakleung@gmail.com>
2019-09-17 00:44:18 -0400

    Add github pages package

---
+++ README.md
@@ -0,0 +1,11 @@
+
+To be completed
+
+- [ ] Firebase Connection
+- [ ] Authentication
+- [ ] React Native Apps
+
+
+
+npm start
+npm run deploy
--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -5,11 +5,12 @@ class Profile extends React.Component {
     render() {
         return (
             <div className="profile">
+                <img src="https://via.placeholder.com/150/" alt="profile" />
                 <h3>Profile </h3>
 
                 <div>
                     <h4>Body Weight</h4>
-                    <div class="row">
+                    <div className="row">
                         <div className="input-field col s6">
                             <i className="material-icons prefix">accessibility</i>
                             <input type="text" id="autocomplete-input" className="autocomplete" />
